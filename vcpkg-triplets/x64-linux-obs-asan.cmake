c5da0bb6fa031e8cb6c5f7a06b41370765299754
syu0519 <syu0519@gmail.com>
2026-04-13 19:06:46 +0800

    Add vcpkg triplets and configuration from main

---
+++ vcpkg-triplets/x64-linux-obs-asan.cmake
@@ -0,0 +1,12 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DCMAKE_INSTALL_DO_STRIP=OFF)
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -fsanitize=address")
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -fsanitize=address")
+set(VCPKG_C_FLAGS_RELEASE "${VCPKG_C_FLAGS_RELEASE} -g -fno-omit-frame-pointer -fstack-protector-strong")
+set(VCPKG_CXX_FLAGS_RELEASE "${VCPKG_CXX_FLAGS_RELEASE} -g -fno-omit-frame-pointer -fstack-protector-strong")
+set(VCPKG_LINKER_FLAGS "${VCPKG_LINKER_FLAGS} -fsanitize=address")
