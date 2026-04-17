7a5f94ff70ea0b300211d439706185eca32cc8eb
syu0519 <syu0519@gmail.com>
2026-04-17 15:29:14 +0800

    test: downgrade onnxruntime to 1.20.1 for RTX 3090 fp16 compatibility

--- cmake/DownloadOnnxruntime.cmake
+++ cmake/DownloadOnnxruntime.cmake
@@ -30,38 +30,38 @@ if(PLATFORM STREQUAL "macos")
 elseif(PLATFORM STREQUAL "windows")
   if(GPU)
     file(
-      DOWNLOAD
-        https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-win-x64-gpu-1.23.2.zip
-        onnxruntime-win-x64-gpu-1.23.2.zip
-      EXPECTED_HASH SHA256=e77afdbbc2b8cb6da4e5a50d89841b48c44f3e47dce4fb87b15a2743786d0bb9
-    )
-    execute_process(
-      COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-gpu-1.23.2.zip
-    )
-    file(RENAME onnxruntime-win-x64-gpu-1.23.2 onnxruntime)
+  DOWNLOAD
+    https://github.com/microsoft/onnxruntime/releases/download/v1.20.1/onnxruntime-win-x64-gpu-1.20.1.zip
+    onnxruntime-win-x64-gpu-1.20.1.zip
+  EXPECTED_HASH SHA256=3e9658d4aa7c21b3f5cbb5a7ce0356184f3c183c317b52f9cfff23a3f079634e
+)
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-gpu-1.20.1.zip
+)
+file(RENAME onnxruntime-win-x64-gpu-1.20.1 onnxruntime)
   else()
     file(
-      DOWNLOAD
-        https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-win-x64-1.23.2.zip
-        onnxruntime-win-x64-1.23.2.zip
-      EXPECTED_HASH SHA256=0b38df9af21834e41e73d602d90db5cb06dbd1ca618948b8f1d66d607ac9f3cd
-    )
-    execute_process(
-      COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-1.23.2.zip
-    )
-    file(RENAME onnxruntime-win-x64-1.23.2 onnxruntime)
+  DOWNLOAD
+    https://github.com/microsoft/onnxruntime/releases/download/v1.20.1/onnxruntime-win-x64-gpu-1.20.1.zip
+    onnxruntime-win-x64-gpu-1.20.1.zip
+  EXPECTED_HASH SHA256=3e9658d4aa7c21b3f5cbb5a7ce0356184f3c183c317b52f9cfff23a3f079634e
+)
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-gpu-1.20.1.zip
+)
+file(RENAME onnxruntime-win-x64-gpu-1.20.1 onnxruntime)
   endif()
 elseif(PLATFORM STREQUAL "linux")
-  file(
-    DOWNLOAD
-      https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-linux-x64-1.23.2.tgz
-      onnxruntime-linux-x64-1.23.2.tgz
-    EXPECTED_HASH SHA256=1fa4dcaef22f6f7d5cd81b28c2800414350c10116f5fdd46a2160082551c5f9b
-  )
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-linux-x64-1.23.2.tgz
-  )
-  file(RENAME onnxruntime-linux-x64-1.23.2 onnxruntime)
+ file(
+  DOWNLOAD
+    https://github.com/microsoft/onnxruntime/releases/download/v1.20.1/onnxruntime-win-x64-gpu-1.20.1.zip
+    onnxruntime-win-x64-gpu-1.20.1.zip
+  EXPECTED_HASH SHA256=3e9658d4aa7c21b3f5cbb5a7ce0356184f3c183c317b52f9cfff23a3f079634e
+)
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E tar xf onnxruntime-win-x64-gpu-1.20.1.zip
+)
+file(RENAME onnxruntime-win-x64-gpu-1.20.1 onnxruntime)
   execute_process(COMMAND ln -s lib onnxruntime/lib64)
 else()
   message(FATAL_ERROR "Unsupported platform: ${PLATFORM}")
